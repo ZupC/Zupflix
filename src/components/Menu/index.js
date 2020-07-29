52bf2cb6153cf3a2baec9fe19c2f18ddb19b32a7
Cairo de souza zupirolli <cairosouza1@gmail.com>
2020-07-29 20:33:27 -0300

    Commit Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
+import { Link } from 'react-router-dom';
 import './menu.css';
 // import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
@@ -7,11 +8,11 @@ import Button from '../Button';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="ZupFlix Logo"></img>
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
       </Button>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/home/App';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+const Pagina404 = () => <div>Página 404</div>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/Cadastro/Video" component={CadastroVideo} />
+      <Route path="/Cadastro/Categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
