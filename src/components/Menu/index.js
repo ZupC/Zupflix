9a180b5865ed0aa9ba535286550884f9bd199c9b
Cairo de souza zupirolli <cairosouza1@gmail.com>
2020-07-29 10:55:04 -0300

    Criando os components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './menu.css';
+import ButtonLink from './components/ButtonLink';
 
 function Menu() {
   return (
-    <nav>
-       ZupFlix show!
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="ZupFlix Logo"></img>
+      </a>
+
+      <ButtonLink className="ButtonLink" href="/">
+        Novo Vídeo
+      </ButtonLink>
     </nav>
   );
 }
