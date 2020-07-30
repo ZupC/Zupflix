64e173b196a32a1822a64c7c1849c4cefcaeda8e
Cairo de souza zupirolli <cairosouza1@gmail.com>
2020-07-30 12:02:55 -0300

    Commit 48:42

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(['teste']);
+  const valoresIniciais = {
+    nome: 'teste',
+    descricao: 'teste desc',
+    Cor: '#000',
+  };
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setvalue(key, value) {
+    setValues({
+      ...values,
+      [key]: value,
+    });
+  }
+  function handleChange(e) {
+    setvalue(e.target.getAttribute('name'), e.target.value);
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <form>
-        <label>
-          Nome da Categoria:
-          <input type="text" />
-        </label>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setCategorias([...categorias, nomeDaCategoria]);
+        }}
+      >
+        <div>
+          <label>
+            Nome da Categoria:
+            <input type="text" name="nome" value={values.nome} onChange={handleChange()} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Descricão:
+            <textarea type="text" name="descricao" value={values.descricao} onChange={handleChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Cor:
+            <input type="color" name="cor" value={values.Cor} onChange={handleChange} />
+          </label>
+        </div>
 
         <button>Cadastrar</button>
       </form>
-
-      <Link to="/">Cadastrar Categoria</Link>
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return <li key={`${categoria}${indice}`}>{categoria}</li>;
+        })}
+      </ul>
+      <Link to="/">Ir para a home</Link>
     </PageDefault>
   );
 }
